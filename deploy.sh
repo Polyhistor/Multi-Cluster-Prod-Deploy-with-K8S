04d93de70935407a0aa80d14a6cbf9a1bdf40121
Polyhistor <pouya.ataei.7@gmail.com>
2021-01-25 21:05:19 +1300

    added comment to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@ docker build -t polyhistor/multi-client:latest -t polyhistor/multi-client:$SHA -
 docker build -t polyhistor/multi-server:latest -t polyhistor/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t polyhistor/multi-worker -t polyhistor/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+# deplying the latest version for devs utilising local env
 docker push polyhistor/multi-client:latest
 docker push polyhistor/multi-server:latest
 docker push polyhistor/multi-worker:latest
 
+# deploying the git versioned image
 docker push polyhistor/multi-client:$SHA
 docker push polyhistor/multi-server:$SHA
 docker push polyhistor/multi-worker:$SHA
